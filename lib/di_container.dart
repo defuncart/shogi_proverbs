400dc9bbc1f08863bee9bac0d33c2423de57b197
James Leahy <defuncart@gmail.com>
2020-07-09 11:08:46 +0200

    Add DIContainer.

---
+++ lib/di_container.dart
@@ -0,0 +1,17 @@
+import 'package:get_it/get_it.dart';
+import 'package:shogi_proverbs/services/settings_database/i_settings_database.dart';
+import 'package:shogi_proverbs/services/settings_database/settings_database.dart';
+
+abstract class DIContainer {
+  static final _getIt = GetIt.instance;
+  static bool _hasBeenSetup = false;
+
+  static void setup() {
+    if (!_hasBeenSetup) {
+      _getIt.registerSingleton<ISettingsDatabase>(SettingsDatabase());
+      _hasBeenSetup = true;
+    }
+  }
+
+  static T get<T>() => _getIt.get<T>();
+}
