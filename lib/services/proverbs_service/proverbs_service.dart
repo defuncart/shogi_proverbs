91f985c9eecd91ac86928a3b18d73927b4597696
James Leahy <defuncart@gmail.com>
2020-07-13 11:29:35 +0200

    Add 1st pawn proverb

--- lib/services/proverbs_service/proverbs_service.dart
+++ lib/services/proverbs_service/proverbs_service.dart
@@ -1,13 +1,14 @@
 import 'package:shogi_proverbs/enums/proverb_type.dart';
 import 'package:shogi_proverbs/models/proverb.dart';
 import 'package:shogi_proverbs/services/proverbs_service/proverbs/lance_proverbs.dart';
-import 'package:shogi_proverbs/services/proverbs_service/proverbs/opening_proverbs.dart';
+import 'package:shogi_proverbs/services/proverbs_service/proverbs/pawn_proverbs.dart';
 
 /// A service to handle proverbs
 class ProverbsService {
   static final _proverbs = {
+    ProverbType.pawn: pawnProverbs,
     ProverbType.lance: lanceProverbs,
-    ProverbType.opening: openingProverbs,
+    // ProverbType.opening: openingProverbs,
   };
 
   /// A map of ProverbType to a list of proverbs
