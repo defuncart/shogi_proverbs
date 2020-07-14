3765d3bfd0cdcc19b38515806be6b9667fc320fa
James Leahy <defuncart@gmail.com>
2020-07-14 09:02:41 +0200

    Add ExternalLinks.

---
+++ lib/configs/external_links.dart
@@ -0,0 +1,5 @@
+/// A config of external links that the app can open
+abstract class ExternalLinks {
+  static const privacyPolicy = 'http://defuncart.com/apps/shogiproverbs/privacypolicy';
+  static const sourceCode = 'https://github.com/defuncart/shogi_proverbs';
+}
