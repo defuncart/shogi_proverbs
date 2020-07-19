4f52dbce7736d0085e1e7d2e1b3f3d7b7a30b568
James Leahy <defuncart@gmail.com>
2020-07-19 07:57:49 +0200

    Add CustomButton.

---
+++ lib/widgets/common/buttons/custom_button.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+/// A button which uses the current theme to style itself
+///
+/// Text color matches scaffold, while button color matches accent
+class CustomButton extends StatelessWidget {
+  final String label;
+  final void Function() onPressed;
+
+  const CustomButton({
+    Key key,
+    @required this.label,
+    @required this.onPressed,
+  })  : assert(label != null),
+        assert(onPressed != null),
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return RaisedButton(
+      color: Theme.of(context).accentColor,
+      child: Text(
+        label,
+        style: TextStyle(
+          color: Theme.of(context).scaffoldBackgroundColor,
+        ),
+      ),
+      onPressed: onPressed,
+    );
+  }
+}
