0d3cf36629936ff82553bf0fa32714e66a3d7170
James Leahy <defuncart@gmail.com>
2020-07-20 08:01:29 +0200

    Rename OnboardingPage to ContentPage. Move to common.

--- lib/widgets/onboarding/pages/onboarding_page_1.dart
+++ lib/widgets/onboarding/pages/onboarding_page_1.dart
@@ -1,41 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:shogi_proverbs/localizations.dart';
-import 'package:shogi_proverbs/widgets/onboarding/pages/onboarding_page.dart';
+import 'package:shogi_proverbs/widgets/common/pages/content_page.dart';
 
 class OnboardingPage1 extends StatelessWidget {
   const OnboardingPage1({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return OnboardingPage(
+    return ContentPage(
       headline: AppLocalizations.onboardingPage1Headline,
       content: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text(
-            AppLocalizations.onboardingPage1Label1,
-            textAlign: TextAlign.justify,
-          ),
-          SizedBox(height: 16.0),
-          Text(
-            AppLocalizations.onboardingPage1Label2,
-            textAlign: TextAlign.justify,
-          ),
-          SizedBox(height: 16.0),
-          Text(
-            AppLocalizations.onboardingPage1Label3,
-            textAlign: TextAlign.justify,
-          ),
-          SizedBox(height: 16.0),
-          Text(
-            AppLocalizations.onboardingPage1Label4,
-            textAlign: TextAlign.justify,
-          ),
-          SizedBox(height: 16.0),
-          Text(
-            AppLocalizations.onboardingPage1Label5,
-            textAlign: TextAlign.justify,
-          ),
+          Text(AppLocalizations.onboardingPage1Label1),
+          Container(height: 16.0),
+          Text(AppLocalizations.onboardingPage1Label2),
+          Container(height: 16.0),
+          Text(AppLocalizations.onboardingPage1Label3),
+          Container(height: 16.0),
+          Text(AppLocalizations.onboardingPage1Label4),
+          Container(height: 16.0),
+          Text(AppLocalizations.onboardingPage1Label5),
         ],
       ),
     );
