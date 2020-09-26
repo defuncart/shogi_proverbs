0fa69600ce40d9b1b8185a5217829e5bd2b72b66
James Leahy <defuncart@gmail.com>
2020-09-26 14:28:02 +0200

    HomeScreen: Update bottom navigation color to match scaffoldBackgroundColor.

--- lib/widgets/home_screen/home_screen.dart
+++ lib/widgets/home_screen/home_screen.dart
@@ -20,22 +20,25 @@ class _HomeScreenState extends State<HomeScreen> {
         title: Text(AppLocalizations.appTitle),
       ),
       body: _currentIndex == 0 ? ProverbsTab() : SettingsTab(),
-      bottomNavigationBar: BottomNavigationBar(
-        items: [
-          _ShiftingBottomNavBarItem(
-            icon: Icons.book,
-            label: AppLocalizations.homeScreenProversTabTitleText,
-            color: _currentIndex == 0 ? Theme.of(context).accentColor : Theme.of(context).disabledColor,
-          ),
-          _ShiftingBottomNavBarItem(
-            icon: Icons.settings,
-            label: AppLocalizations.homeScreenSettingsTabTitleText,
-            color: _currentIndex == 1 ? Theme.of(context).accentColor : Theme.of(context).disabledColor,
-          ),
-        ],
-        currentIndex: _currentIndex,
-        onTap: (index) => setState(() => _currentIndex = index),
-        type: BottomNavigationBarType.shifting,
+      bottomNavigationBar: Theme(
+        data: Theme.of(context).copyWith(canvasColor: Theme.of(context).scaffoldBackgroundColor),
+        child: BottomNavigationBar(
+          items: [
+            _ShiftingBottomNavBarItem(
+              icon: Icons.book,
+              label: AppLocalizations.homeScreenProversTabTitleText,
+              color: _currentIndex == 0 ? Theme.of(context).accentColor : Theme.of(context).disabledColor,
+            ),
+            _ShiftingBottomNavBarItem(
+              icon: Icons.settings,
+              label: AppLocalizations.homeScreenSettingsTabTitleText,
+              color: _currentIndex == 1 ? Theme.of(context).accentColor : Theme.of(context).disabledColor,
+            ),
+          ],
+          currentIndex: _currentIndex,
+          onTap: (index) => setState(() => _currentIndex = index),
+          type: BottomNavigationBarType.shifting,
+        ),
       ),
     );
   }
