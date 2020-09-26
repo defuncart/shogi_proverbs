86872bfd80e83b227bbccfdc970f605a3c36e60a
James Leahy <defuncart@gmail.com>
2020-09-26 14:29:53 +0200

    Update AppThemes with prototype colors.

--- lib/configs/app_themes.dart
+++ lib/configs/app_themes.dart
@@ -1,7 +1,49 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 /// A config of themes used in the app
 class AppThemes {
-  static final light = ThemeData.light();
-  static final dark = ThemeData.dark();
+  static const _lightAppColors = _AppColors.light();
+
+  static final light = ThemeData.light().copyWith(
+    scaffoldBackgroundColor: _lightAppColors.scaffoldBackgroundColor,
+    accentColor: _lightAppColors.accentColor,
+    appBarTheme: AppBarTheme(
+      color: _lightAppColors.appBarColor,
+    ),
+    //TODO this is because _PackagesView from about.dart uses card color for background color
+    cardColor: _lightAppColors.scaffoldBackgroundColor,
+  );
+
+  static const _darkAppColors = _AppColors.dark();
+
+  static final dark = ThemeData.dark().copyWith(
+    scaffoldBackgroundColor: _darkAppColors.scaffoldBackgroundColor,
+    accentColor: _darkAppColors.accentColor,
+    appBarTheme: AppBarTheme(color: _darkAppColors.appBarColor),
+    toggleableActiveColor: _darkAppColors.accentColor,
+    disabledColor: _darkAppColors.disabledColor,
+    //TODO this is because _PackagesView from about.dart uses card color for background color
+    cardColor: _darkAppColors.scaffoldBackgroundColor,
+  );
+}
+
+class _AppColors {
+  final Color scaffoldBackgroundColor;
+  final Color accentColor;
+  final Color disabledColor;
+  final Color appBarColor;
+
+  const _AppColors.light()
+      : scaffoldBackgroundColor = const Color(0xffe2e3d1),
+        accentColor = const Color(0xff474838),
+        disabledColor = const Color(0xffafafaf),
+        appBarColor = const Color(0xff44423d);
+
+  const _AppColors.dark()
+      : scaffoldBackgroundColor = const Color(0xff252525),
+        accentColor = const Color(0xfffc856d),
+        disabledColor = const Color(0xffafafaf),
+        appBarColor = const Color(0xff131b20);
 }
