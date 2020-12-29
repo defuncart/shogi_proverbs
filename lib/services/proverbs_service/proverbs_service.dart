1a2ba89e21d234a0a34ba074e4e023fa51dc803c
James Leahy <defuncart@gmail.com>
2020-12-29 16:00:06 +0100

    Add opening 4.

--- lib/services/proverbs_service/proverbs_service.dart
+++ lib/services/proverbs_service/proverbs_service.dart
@@ -10,6 +10,7 @@ import 'package:shogi_proverbs/services/proverbs_service/proverbs/individual_pie
 import 'package:shogi_proverbs/services/proverbs_service/proverbs/individual_pieces/silver_proverbs.dart';
 import 'package:shogi_proverbs/services/proverbs_service/proverbs/individual_pieces/tokin_proverbs.dart';
 import 'package:shogi_proverbs/services/proverbs_service/proverbs/opening_proverbs/opening_proverbs13.dart';
+import 'package:shogi_proverbs/services/proverbs_service/proverbs/opening_proverbs/opening_proverbs46.dart';
 
 /// A service to handle proverbs
 class ProverbsService {
@@ -27,6 +28,7 @@ class ProverbsService {
     ],
     ProverbType.opening: [
       ...openingProverbs13,
+      ...openingProverbs46,
     ],
   };
 
