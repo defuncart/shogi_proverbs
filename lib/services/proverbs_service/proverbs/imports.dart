20a015a9c929f2f00c7ff2c4a595b714bf7004f1
James Leahy <defuncart@gmail.com>
2020-12-31 10:18:05 +0100

    Add imports for all proverbs files.

---
+++ lib/services/proverbs_service/proverbs/imports.dart
@@ -0,0 +1,7 @@
+export 'package:shogi/shogi.dart';
+export 'package:shogi_proverbs/enums/proverb_type.dart';
+export 'package:shogi_proverbs/models/page.dart';
+export 'package:shogi_proverbs/models/proverb.dart';
+export 'package:shogi_proverbs/models/sections/diagram.dart';
+export 'package:shogi_proverbs/models/sections/move_sequence.dart' hide PlayerTypeExtensions;
+export 'package:shogi_proverbs/models/sections/paragraph.dart';
