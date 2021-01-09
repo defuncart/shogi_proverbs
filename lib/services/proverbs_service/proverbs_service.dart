21919a45b1f00c8964789148b0cdf72e2376116c
James Leahy <defuncart@gmail.com>
2021-01-09 16:11:58 +0100

    Update ProverbsService to be able to search.

--- lib/services/proverbs_service/proverbs_service.dart
+++ lib/services/proverbs_service/proverbs_service.dart
@@ -13,21 +13,35 @@ import 'package:shogi_proverbs/services/proverbs_service/proverbs/opening_prover
 
 /// A service to handle proverbs
 class ProverbsService {
-  static final _proverbs = {
-    ProverbType.pieces: [
-      ...pawnProverbs,
-      ...lanceProverbs,
-      ...knightProverbs,
-      ...silverProverbs,
-      ...goldProverbs,
-      ...bishopProverbs,
-      ...horseProverbs,
-      ...dragonProverbs,
-      ...tokinProverbs,
-    ],
-    ProverbType.opening: openingProverbs,
-  };
+  static final _proverbs = [
+    ...pawnProverbs,
+    ...lanceProverbs,
+    ...knightProverbs,
+    ...silverProverbs,
+    ...goldProverbs,
+    ...bishopProverbs,
+    ...horseProverbs,
+    ...dragonProverbs,
+    ...tokinProverbs,
+    ...openingProverbs,
+  ];
 
   /// A map of ProverbType to a list of proverbs
-  static Map<ProverbType, List<Proverb>> get proverbs => _proverbs;
+  static Map<ProverbType, List<Proverb>> get proverbs => _mapSelectedProverbsToType(_proverbs);
+
+  /// Returns proverbs satisfying a filter
+  static Map<ProverbType, List<Proverb>> proverbsWithFilter(String filter) {
+    final proverbs = filter == null || filter.isEmpty
+        ? _proverbs
+        : _proverbs.where((proverbs) => proverbs.title.toLowerCase().contains(filter.toLowerCase())).toList();
+    return _mapSelectedProverbsToType(proverbs);
+  }
+
+  static Map<ProverbType, List<Proverb>> _mapSelectedProverbsToType(List<Proverb> proverbs) {
+    final map = {
+      for (final type in ProverbType.values) type: proverbs.where((proverb) => proverb.type == type).toList(),
+    };
+    map.removeWhere((key, value) => value.isEmpty);
+    return map;
+  }
 }
