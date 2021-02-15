5470a974d12654c41310d92f7ae8a1c2398ab210
James Leahy <defuncart@gmail.com>
2021-02-15 17:35:39 +0100

    Add middlegame 3.

--- lib/services/proverbs_service/proverbs/middlegame_proverbs/middlegame_proverbs.dart
+++ lib/services/proverbs_service/proverbs/middlegame_proverbs/middlegame_proverbs.dart
@@ -1,7 +1,9 @@
 import 'package:shogi_proverbs/services/proverbs_service/proverbs/middlegame_proverbs/middlegame_proverb1.dart';
 import 'package:shogi_proverbs/services/proverbs_service/proverbs/middlegame_proverbs/middlegame_proverb2.dart';
+import 'package:shogi_proverbs/services/proverbs_service/proverbs/middlegame_proverbs/middlegame_proverb3.dart';
 
 const middlegameProverbs = [
   middlegameProverb1,
   middlegameProverb2,
+  middlegameProverb3,
 ];
