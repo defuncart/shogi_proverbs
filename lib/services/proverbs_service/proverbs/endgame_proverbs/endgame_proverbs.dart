2ac2a493ee55664ebf7a432b32c8618358a33e94
James Leahy <defuncart@gmail.com>
2021-07-09 16:04:52 +0200

    Add ending 6.

--- lib/services/proverbs_service/proverbs/endgame_proverbs/endgame_proverbs.dart
+++ lib/services/proverbs_service/proverbs/endgame_proverbs/endgame_proverbs.dart
@@ -3,6 +3,7 @@ import 'package:shogi_proverbs/services/proverbs_service/proverbs/endgame_prover
 import 'package:shogi_proverbs/services/proverbs_service/proverbs/endgame_proverbs/endgame_proverb3.dart';
 import 'package:shogi_proverbs/services/proverbs_service/proverbs/endgame_proverbs/endgame_proverb4.dart';
 import 'package:shogi_proverbs/services/proverbs_service/proverbs/endgame_proverbs/endgame_proverb5.dart';
+import 'package:shogi_proverbs/services/proverbs_service/proverbs/endgame_proverbs/endgame_proverb6.dart';
 
 const endgameProverbs = [
   endgameProverb1,
@@ -10,4 +11,5 @@ const endgameProverbs = [
   endgameProverb3,
   endgameProverb4,
   endgameProverb5,
+  endgameProverb6,
 ];
