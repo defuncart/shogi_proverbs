e8add49c227425ca2ff8046a3de86a68abd5ce63
James Leahy <defuncart@gmail.com>
2021-07-10 14:25:37 +0200

    Add tests for ProverbService.

--- test/services/proverbs_service/proverbs_service_test.dart
+++ test/services/proverbs_service/proverbs_service_test.dart
@@ -3,15 +3,31 @@ import 'package:shogi_proverbs/models/proverb.dart';
 import 'package:shogi_proverbs/services/proverbs_service/proverbs_service.dart';
 
 void main() {
-  final proverbs = ProverbsService.proverbsWithFilter('')
-      .values
-      .fold<List<Proverb>>([], (previousValue, element) => [...previousValue, ...element]);
-  final indeces = proverbs.map((proverb) => proverb.index).toList();
-
   test('Ensure that all indeces are unique', () {
+    final proverbs = ProverbsService.proverbsWithFilter('')
+        .values
+        .fold<List<Proverb>>([], (previousValue, element) => [...previousValue, ...element]);
+    final indeces = proverbs.map((proverb) => proverb.index).toList();
+
     expect(
       indeces.length,
       indeces.toSet().length,
     );
+  });
+
+  test('Ensure that Proverb types are correct', () {
+    final proverbsMap = ProverbsService.proverbsWithFilter('');
+    for (final kvp in proverbsMap.entries) {
+      for (final proverb in kvp.value) {
+        expect(proverb.type, kvp.key);
+      }
+    }
+  });
+
+  test('Ensure that filtering works', () {
+    final proverbs = ProverbsService.proverbsWithFilter('king')
+        .values
+        .fold<List<Proverb>>([], (previousValue, element) => [...previousValue, ...element]);
+    expect(proverbs.length, 10);
   });
 }
