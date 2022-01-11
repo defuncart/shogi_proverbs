3fc05b02b1cca6b613ff5fb50e5175001cdacf10
James Leahy <defuncart@gmail.com>
2022-01-11 19:08:56 +0100

    Add other5

--- lib/services/proverbs_service/proverbs/other_proverbs/other_proverbs.dart
+++ lib/services/proverbs_service/proverbs/other_proverbs/other_proverbs.dart
@@ -2,10 +2,12 @@ import 'package:shogi_proverbs/services/proverbs_service/proverbs/other_proverbs
 import 'package:shogi_proverbs/services/proverbs_service/proverbs/other_proverbs/other_proverb2.dart';
 import 'package:shogi_proverbs/services/proverbs_service/proverbs/other_proverbs/other_proverb3.dart';
 import 'package:shogi_proverbs/services/proverbs_service/proverbs/other_proverbs/other_proverb4.dart';
+import 'package:shogi_proverbs/services/proverbs_service/proverbs/other_proverbs/other_proverb5.dart';
 
 const otherProverbs = [
   otherProverb1,
   otherProverb2,
   otherProverb3,
   otherProverb4,
+  otherProverb5,
 ];
