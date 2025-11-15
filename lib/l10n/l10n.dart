891e8397b58ec5b9702561dfd852015a36957b6e
James Leahy <defuncart@gmail.com>
2025-11-15 12:08:03 +0100

    chore: Update flutter 3.38 (#37)

--- lib/l10n/l10n.dart
+++ lib/l10n/l10n.dart
@@ -2,5 +2,5 @@ import 'package:flutter/material.dart';
 import 'package:shogi_proverbs/l10n/generated/localizations.dart';
 
 extension L10nExtension on BuildContext {
-  AppLocalizations get l10n => AppLocalizations.of(this)!;
+  AppLocalizations get l10n => AppLocalizations.of(this);
 }
